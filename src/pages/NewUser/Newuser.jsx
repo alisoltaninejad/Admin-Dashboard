475babf8d3c910d675bd77498703863fea3d7161
Ali Soltani <alisolinejad@gmail.com>
2025-08-11 10:18:46 +0330

    NewUser page update & remove q file

--- src/pages/NewUser/Newuser.jsx
+++ src/pages/NewUser/Newuser.jsx
@@ -4,22 +4,27 @@ import Icon from "../../components/Icons/Icons";
 export default function Newuser() {
   return (
     <>
-      <div className="w-full h-[350px] flex justify-center items-center ">
-        <div className='relative'>
-          <Icon name="profile" color="brand" className="w-[300px] h-[300px]" />
-          <Icon name="upload" color="danger" 
-          className='w-[50px] h-[50px] absolute bottom-6 end-10 bg-white rounded-full p-1.5 ' />
-          
-        </div>
-
-        <div className=" flex flex-col justify-center items-center">
+      <div className="w-[65%] h-[350px] flex justify-evenly items-center gap-6 mx-auto ">
+        {/* {inputs section } */}
+        <div className=" flex flex-col justify-center items-center p-6">
           <Input name="name" text="نام" />
           <Input name="email" text="ایمیل" />
           <Input name="phone" text="تلفن" />
           <Input name="job" text="شغل" />
         </div>
+        {/* {profile pic avatar} */}
+        <div className="relative">
+          <Icon name="profile" color="brand" className="w-[300px] h-[300px]" />
+          <Icon
+            name="upload"
+            color="danger"
+            className="w-[50px] h-[50px] absolute bottom-6 end-10 bg-white rounded-full p-1.5 "
+          />
+        </div>
       </div>
-      <button className="block w-[800px]  h-8 mt-4 mx-auto rounded-md bg-brand-800 text-white cursor-pointer">ثبت اطلاعات</button>
+      <button className="block w-[65%]  h-8 mt-4 mx-auto rounded-md bg-brand-800 text-white cursor-pointer">
+        ثبت اطلاعات
+      </button>
     </>
   );
 }
