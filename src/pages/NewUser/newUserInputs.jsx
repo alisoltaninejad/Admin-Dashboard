d903e6b8b9a6fbd5436a7657bb84f2943f87a6ea
Ali Soltani <alisolinejad@gmail.com>
2025-08-11 12:11:08 +0330

    add comments to userModule

--- src/pages/NewUser/newUserInputs.jsx
+++ src/pages/NewUser/newUserInputs.jsx
@@ -1,11 +1,13 @@
-export default function NewUserInputs({name, text, type = 'text'}) {
+export default function NewUserInputs({ name, text, type = "text" }) {
   return (
-    <div className='flex items-center my-3 gap-4'>
-      <label htmlFor={name} className='w-24 text-left'>{text}:</label>
-      <input 
-        type={type} 
-        name={name} 
-        className='flex-1 grow h-10  rounded-md shadow shadow-gray-700'
+    <div className="flex items-center my-3 gap-4">
+      <label htmlFor={name} className="w-6 mx-2 text-left">
+        {text}:
+      </label>
+      <input
+        type={type}
+        name={name}
+        className="flex-1 grow h-10  rounded-md shadow shadow-gray-700"
       />
     </div>
   );
