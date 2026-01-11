7b86ce0c161e7db5b0bb1e678ac43a15d8dce1f2
Ali Soltani <alisolinejad@gmail.com>
2026-01-11 20:14:27 +0330

    style(Users): add scroll to users-table in Users Page

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -121,8 +121,8 @@ export default function Users() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="bg-white shadow-md rounded-lg overflow-hidden">
-        <table className="min-w-full divide-y divide-gray-200">
+      <div className="bg-white shadow-md rounded-lg overflow-x-scroll ">
+        <table className="min-w-full divide-y divide-gray-200  ">
           <thead className="bg-gray-50">
             <tr>
               <th className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
