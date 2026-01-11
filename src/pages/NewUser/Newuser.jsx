f6bd7c12d931fc8770c17306786c4b66dda0e686
Ali Soltani <alisolinejad@gmail.com>
2026-01-11 20:03:01 +0330

    feat(NewUser): responsive ui for adding NewUser page

--- src/pages/NewUser/Newuser.jsx
+++ src/pages/NewUser/Newuser.jsx
@@ -4,25 +4,26 @@ import Icon from "../../components/Icons/Icons";
 export default function Newuser() {
   return (
     <>
-      <div className="w-[65%] h-[350px] flex justify-evenly items-center gap-6 mx-auto ">
+      <div className="min-w-fit min-h-fit w-[65%] h-full flex flex-col md:flex-row justify-center items-center md:gap-6 mx-auto md:mt-12 ">
+        {/* {profile pic avatar} */}
+        <div className="relative md:order-2 mx-auto">
+          
+          <Icon name="profile" color="brand" className="w-[200px] h-[200px]  md:w-[300px] md:h-[300px]" />
+          <Icon
+            name="upload"
+            color="danger"
+            className="md:w-[50px] md:h-[50px] w-[40px] h-[40px]  absolute bottom-6 end-6 bg-white rounded-full p-2 "
+          />
+        </div>
         {/* {inputs section } */}
-        <div className=" flex flex-col justify-center items-center p-6">
+        <div className=" flex md:order-1 flex-col justify-center items-center p-6">
           <Input name="name" text="نام" />
           <Input name="email" text="ایمیل" />
           <Input name="phone" text="تلفن" />
           <Input name="job" text="شغل" />
         </div>
-        {/* {profile pic avatar} */}
-        <div className="relative">
-          <Icon name="profile" color="brand" className="w-[300px] h-[300px]" />
-          <Icon
-            name="upload"
-            color="danger"
-            className="w-[50px] h-[50px] absolute bottom-6 end-10 bg-white rounded-full p-1.5 "
-          />
-        </div>
       </div>
-      <button className="block w-[65%]  h-8 mt-4 mx-auto rounded-md bg-brand-800 text-white cursor-pointer">
+      <button className="block w-80 h-8  md:w-[65%] md:h-10 md:mt-4 mx-auto rounded-md bg-brand-800 text-white cursor-pointer">
         ثبت اطلاعات
       </button>
     </>
--- src/pages/NewUser/newUserInputs.jsx
+++ src/pages/NewUser/newUserInputs.jsx
@@ -1,13 +1,13 @@
 export default function NewUserInputs({ name, text, type = "text" }) {
   return (
-    <div className="flex items-center my-3 gap-4">
-      <label htmlFor={name} className="w-6 mx-2 text-left">
+    <div className="flex items-center w-70 md:w-60 my-3 gap-3">
+      <label htmlFor={name} className="w-6 text-left">
         {text}:
       </label>
       <input
         type={type}
         name={name}
-        className="flex-1 grow h-10  rounded-md shadow shadow-gray-700"
+        className="flex-1 grow h-8 rounded-md shadow shadow-gray-700"
       />
     </div>
   );
