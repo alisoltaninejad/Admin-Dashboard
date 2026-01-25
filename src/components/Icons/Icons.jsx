a487fcbc5cb7cb3ab43697b3f1a85e85cabf7081
Ali Soltani <alisolinejad@gmail.com>
2026-01-25 12:05:44 +0330

    style(Icons): change brand color for Icons

--- src/components/Icons/Icons.jsx
+++ src/components/Icons/Icons.jsx
@@ -72,7 +72,7 @@ const IconSystem = ({ name, size = "md", color = "primary", className = "" }) =>
     danger: "text-red-500 dark:text-red-400",
     success: "text-green-500 dark:text-green-400",
     dark: "dark:text-black text-white",
-    brand: "text-brand-700",
+    brand: "text-brand-500 ",
   };
 
   const IconComponent = icons[name];
