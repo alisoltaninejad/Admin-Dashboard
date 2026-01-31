f5767ed7caf8fe9c4990994f05a656ef113ab480
Ali Soltani <alisolinejad@gmail.com>
2026-01-31 17:59:34 +0330

    style: :art: handle theme for loading in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 
   if (!dbReady) {
     return (
-      <div className="fixed inset-0 flex flex-col items-center justify-center bg-white z-50">
+      <div className="fixed inset-0 flex flex-col items-center justify-center text-shadow-brand-800 bg-brand-50  z-50">
         <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-blue-500"></div>
         <p className="mt-4 text-gray-600">در حال راه‌اندازی سیستم...</p>
       </div>
