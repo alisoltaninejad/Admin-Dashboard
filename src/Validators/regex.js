48fc310cfa3869b7445c87f4b4b4f27e501928be
Ali Soltani <alisolinejad@gmail.com>
2026-02-01 16:31:15 +0330

    feat(global): create validators
    includinmg rules & regexes
    
    plus a using module

---
+++ src/Validators/regex.js
@@ -0,0 +1,13 @@
+const testEmail = (value) => {
+    const emailPattent = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/g
+    return emailPattent.test(value)
+}
+
+const testPhoneNumber = (value) => {
+    // regex
+}
+
+export default {
+    testEmail,
+    testPhoneNumber
+}
