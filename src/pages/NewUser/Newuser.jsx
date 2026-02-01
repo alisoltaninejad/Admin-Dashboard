c29099cf70c5057340c3973a9c807cef15f473f1
Ali Soltani <alisolinejad@gmail.com>
2026-02-01 16:38:08 +0330

    refactor(NewUser): use new components

--- src/pages/NewUser/Newuser.jsx
+++ src/pages/NewUser/Newuser.jsx
@@ -1,27 +1,42 @@
 import { useRef } from "react";
-import Input from "./newUserInputs";
 import Icon from "../../components/Icons/Icons";
+import Button from "../../components/ui/Button";
+import Input from "../../components/ui/Input";
+import { useForm } from "../../Hooks/useForm";
+import {
+  requiredValidator,
+  minValidator,
+  maxValidator,
+  emailValidator,
+} from "../../validators/rules";
 
 export default function Newuser() {
   const inputRefs = useRef([]);
   
-  // هندلر کلید Enter
-  const handleKeyDown = (index, e) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-      const nextIndex = index + 1;
-      if (nextIndex < inputRefs.current.length && inputRefs.current[nextIndex]) {
-        inputRefs.current[nextIndex].focus();
-      }
-    }
-  };
+  const [formState, onInputHandler] = useForm(
+    {
+      username: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false,
+  );
   
   return (
     <>
       <div className="min-w-fit min-h-fit w-[65%] flex flex-col md:flex-row justify-center items-center md:gap-6 mx-auto mt-5 md:mt-16 ">
         {/* {profile pic avatar} */}
         <div className="relative md:order-2 mx-auto">
-          <Icon name="profile" color="brand" className="w-[200px] h-[200px] md:w-[300px] md:h-[300px]" />
+          <Icon
+            name="profile"
+            color="brand"
+            className="w-[200px] h-[200px] md:w-[300px] md:h-[300px]"
+          />
           <Icon
             name="upload"
             color="danger"
@@ -31,39 +46,67 @@ export default function Newuser() {
         
         {/* {inputs section } */}
         <div className="flex md:order-1 flex-col justify-center items-center p-6">
-          <Input 
+          <Input
+            element="input"
             name="name" 
-            text="نام" 
+            id="username"
+            lable="نام"
+            placeholder="علی سلطانی نژاد"
             autoFocus={true}
-            inputRef={(el) => inputRefs.current[0] = el}
-            onKeyDown={(e) => handleKeyDown(0, e)}
-          />
+            required={true}
+            validations={[
+              requiredValidator(),
+              minValidator(3),
+              maxValidator(30),
+            ]}
+            onInputHandler={onInputHandler}></Input>
           <Input 
+            element="input"
             name="email" 
-            text="ایمیل"
-            inputRef={(el) => inputRefs.current[1] = el}
-            onKeyDown={(e) => handleKeyDown(1, e)}
-          />
+            id="email"
+            lable="ایمیل"
+            required={true}
+            placeholder="alosolinejad@gmail.com"
+            validations={[
+              requiredValidator(),
+              minValidator(4),
+              maxValidator(30),
+              emailValidator(),
+            ]}
+            onInputHandler={onInputHandler}></Input>
           <Input 
+            element="input"
             name="phone" 
-            text="تلفن"
-            inputRef={(el) => inputRefs.current[2] = el}
-            onKeyDown={(e) => handleKeyDown(2, e)}
-          />
+            id="phone"
+            lable="تلفن"
+            required={true}
+            placeholder="09135006973"
+            validations={[
+              requiredValidator(),
+              minValidator(11),
+              maxValidator(11),
+            ]}
+            onInputHandler={onInputHandler}></Input>
           <Input 
+            element="input"
             name="job" 
-            text="شغل"
-            inputRef={(el) => inputRefs.current[3] = el}
-            onKeyDown={(e) => handleKeyDown(3, e)}
-          />
+            id="job"
+            lable="شغل"
+            placeholder="انتخاب از لیست"
+            validations={[
+              requiredValidator(),
+              minValidator(11),
+              maxValidator(11),
+            ]}
+            onInputHandler={onInputHandler}></Input>
         </div>
       </div>
-      <button 
-        type="button"
-        className="block w-80 h-8 md:w-[65%] md:h-10 md:mt-4 mx-auto rounded-md bg-brand-600 hover:bg-brand-700 dark:hover:bg-brand-400 transition-colors text-white cursor-pointer"
-      >
+      <Button
+        type="submit"
+        disabled={true}
+        className="block w-80 h-8 md:w-[65%] md:h-10 md:mt-4 mx-auto rounded-md bg-brand-600 hover:bg-brand-700 dark:hover:bg-brand-400 transition-all duration-200 text-white cursor-pointer disabled:opacity-70 disabled:cursor-not-allowed disabled:hover:bg-brand-600 disabled:dark:hover:bg-brand-600 disabled:transform-none disabled:shadow-none">
         ثبت اطلاعات
-      </button>
+      </Button>
     </>
   );
 }
