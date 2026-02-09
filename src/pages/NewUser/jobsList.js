06f7aa2977be94cd4420d236e0340168922050c4
Ali Soltani <alisolinejad@gmail.com>
2026-02-09 16:03:52 +0330

    feat(NewUser): create jobs list Data

---
+++ src/pages/NewUser/jobsList.js
@@ -0,0 +1,9 @@
+export default [
+  { id: 1, label: "product manager", value: "product-manager" },
+  { id: 2, label: "frontend developer", value: "frontend-developer" },
+  { id: 3, label: "backend developer", value: "backend-developer" },
+  { id: 4, label: "mobile developer", value: "mobile-developer" },
+  { id: 5, label: "ui/ux designer", value: "ui/ux-designer" },
+  { id: 6, label: "graphic designer", value: "graphic-designer" },
+  { id: 7, label: "product designer", value: "product-designer" },
+];
